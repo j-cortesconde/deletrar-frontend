ae8fec8314c23d6de7382ec48df79e1c4c0ff6e7
j-cortesconde <jcortesconde@gmail.com>
2024-04-10 12:45:23 -0300

    Tailwind intialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import AppLayout from "./ui/AppLayout";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="home" />} />
+          <Route path="home" element={<AppLayout />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,5 @@
+function AppLayout() {
+  return <div>Hello world!!</div>;
+}
+
+export default AppLayout;
