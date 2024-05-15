fcbbef64bc93a6a08c0fb977cd6790eece8e7b32
j-cortesconde <jcortesconde@gmail.com>
2024-05-15 07:47:20 -0300

    Added prefetching for pagination

--- src/features/posts/usePosts.js
+++ src/features/posts/usePosts.js
@@ -1,10 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getPosts } from "../../services/apiPosts";
 import { useSearchParams } from "react-router-dom";
 import { PAGE_SIZE, POST_SORT_OPTIONS } from "../../utils/constants";
 
 export function usePosts(authorUsername) {
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   const sortBy = searchParams.get("sortBy") || POST_SORT_OPTIONS[0].value;
   const page = Number(searchParams.get("page")) || 1;
@@ -20,6 +21,25 @@ export function usePosts(authorUsername) {
 
   const posts = data?.docs;
   const count = data?.count;
+
+  // PRE-FETCHING of next and previous pages
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  const nextQueryString = `sortBy=${sortBy}&page=${page + 1}&limit=${limit}`;
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["posts", authorUsername, sortBy, page + 1],
+      queryFn: () => getPosts(authorUsername, nextQueryString),
+    });
+
+  const previousQueryString = `sortBy=${sortBy}&page=${page - 1}&limit=${limit}`;
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["posts", authorUsername, sortBy, page - 1],
+      queryFn: () => getPosts(authorUsername, previousQueryString),
+    });
 
   return { isLoading, posts, count, error };
 }
