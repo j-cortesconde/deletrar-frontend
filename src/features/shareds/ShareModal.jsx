0f25917e4bb70c8e0ce970e12371be3b571798ef
j-cortesconde <jcortesconde@gmail.com>
2024-11-07 20:47:37 -0300

    Made minor changes so ShareModal would work better (by itself and on Collections)

--- src/features/shareds/ShareModal.jsx
+++ src/features/shareds/ShareModal.jsx
@@ -31,6 +31,7 @@ function ShareModal({ children, sharedComment }) {
       onSuccess: () => {
         toast.success("Compartido exitosamente");
         closeModal();
+        setContent();
       },
     });
   }
