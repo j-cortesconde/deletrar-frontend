2767d03601e74f841f2b3969035ac9b0c54cb13e
j-cortesconde <jcortesconde@gmail.com>
2024-11-29 11:33:18 -0300

    Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,8 @@
-import { useNavigate } from "react-router-dom";
 import Feed from "../features/feed/Feed";
 
 function Home() {
-  const navigate = useNavigate();
   return (
     <div>
-      <button
-        onClick={() => {
-          navigate("/post/write/673fae512f55e37fbc4cda62");
-        }}
-      >
-        Botoneá
-      </button>
       <Feed />
     </div>
   );
