5c7e617c6449278eb847d59d401cbd169175ec2b
j-cortesconde <jcortesconde@gmail.com>
2025-01-27 19:13:25 -0300

    Fixed env variables for Vite

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -6,7 +6,7 @@
 // TODO: Un logic delete, va como PATCH o como qué?
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 class AxiosService {
   #jwt;
