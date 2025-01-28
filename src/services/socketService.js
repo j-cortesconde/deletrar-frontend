313131beac2dece9b9bca13937f04b3c261ab878
j-cortesconde <jcortesconde@gmail.com>
2025-01-28 17:21:19 -0300

    Added env variable for socket url

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = import.meta.env.VITE_API_URL;
+const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_URL;
 
 class SocketService {
   #jwt;
